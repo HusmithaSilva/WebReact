e914105fefd00d1b91ae15b1ca7d0ae671aeb6da
HusmithaSilva <husmitha.silva0@gmail.com>
2020-03-25 12:08:40 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Table from "./component/table";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      data: 0
+    };
+  }
+
+  onClick  = (event, type) => {
+    if (type === '-'){
+        this.setState({
+            data: this.state.data - 1
+    });
+    }else{
+        this.setState({
+            data: this.state.data + 1
+        });
+    }
+  };
+
+  render() {
+    return(
+        <div className={"container"}>
+            <div className={"row"}>
+              <div className={"col-md-12"}>
+                <div className={"text-center mt-5"}>
+                    <h1>{this.state.data}</h1>
+
+                    <button className="btn btn-primary mr-3" onClick={(event) => this.onClick(event)}>-</button>
+
+                    <button className="btn btn-primary" onClick={(event) => this.onClick(event)}>+</button>
+
+                    <Table></Table>
+
+
+                </div>
+              </div>
+            </div>
+        </div>
+    )
+  }
 }
-
 export default App;
---
+++ src/component/TableHead.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const TableHead = (props) => {
+    return(
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+    )
+};
+
+export default TableHead;
